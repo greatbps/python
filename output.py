45346111b645be17e59d06d26ff9442084a10252
greatbps <greatbps@gmail.com>
2023-03-12 16:42:45 +0900

    ㅇㅇ

---
+++ output.py
@@ -0,0 +1,13 @@
+print("#하나만 출력")
+print("Hello python programming....!")
+print()
+
+print("#여러개 출력")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의 이름은", "이준한입니다.")
+
+print("#아무것도 출력하지 않음")
+print("-----확인 전용선--------")
+print()
+print()
+print("-----확인 전용선--------")
